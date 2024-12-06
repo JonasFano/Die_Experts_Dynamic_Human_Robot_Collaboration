fbcc5dc6fa8d6113efa3fc0247946b06ab3de8fe
henrik@schwrz.dk <henrik@schwrz.dk>
2024-12-06 23:03:00 +0100

    robot controller

--- components/safety-monitor/socket-robot-controller/events.py
+++ components/safety-monitor/socket-robot-controller/events.py
@@ -1,19 +1,23 @@
 from enum import Enum
 from typing import Dict
 
+
 class Events(Enum):
     IS_CONNECTED = "connect"
-    MOVE_TO_POSITION = "move_to_position"
-    MOVE_TO_POSITION_L = "move_to_position_l"
-    MOVE_TO_POSITION_BLEND = "move_to_postion_blend"
-    GET_JOINT_POSITIONS = "get_joint_positions"
+    IS_RUNNING = "is_running"
     STOP_ROBOT = "stop_robot"
     SERVER_MESSAGE = "server_message"
     JOINT_POSITIONS = "joint_positions"
-    IS_RUNNING = "is_running"
-    SPEED_VALUE = "speed_value"
+    GET_SPEED_VALUE = "get_speed_value"
+    SET_SPEED_VALUE = "set_speed_value"
     GET_TCP_VALUE = "get_tcp_value"
     SET_GRIPPER = "set_gripper_value"
+    GET_JOINT_POSITIONS = "get_joint_positions"
+    MOVE_TO_POSITION = "move_to_position"
+    MOVE_TO_POSITION_L = "move_to_position_l"
+    MOVE_TO_POSITION_J = "move_to_position_l"
+    MOVE_L_SPEED_ACCEL = "move_l_SPEED_ACCEL"
+
 
 menu_options: Dict[int, str] = {
     0: "Disconnect",
@@ -25,5 +29,5 @@ menu_options: Dict[int, str] = {
     6: "Robot status",
     7: "Stop moving",
     8: "Open gripper",
-    9: "Close gripper"
+    9: "Close gripper",
 }
--- components/safety-monitor/socket-robot-controller/server.py
+++ components/safety-monitor/socket-robot-controller/server.py
@@ -1,138 +1,124 @@
+import os
+import signal
 import socketio
 import rtde_control
 import rtde_receive
-import rtde_io
+import rtde_io as r_io
 from events import Events  # Import the shared enum
+from typing import List
+import numpy as np
+from flask import Flask
 
+robot_enabled = False
 ROBOT_SPEED = 0.5
 
-# Initialize Socket.IO server
-sio = socketio.Server(async_mode='eventlet')
-app = socketio.WSGIApp(sio)
-
+app = Flask(__name__)
+sio = socketio.Server(cors_allowed_origins="*")
+app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
 # Robot control variables
-ROBOT_HOST = "192.168.1.100"  # Replace with your robot's IP
-rtde_control = rtde_control.RTDEControlInterface(ROBOT_HOST)
-rtde_receive = rtde_receive.RTDEReceiveInterface(ROBOT_HOST)
-i_rtde_io = rtde_io.RTDEIOInterface(ROBOT_HOST)
-i_rtde_io.setSpeedSlider(ROBOT_SPEED)
+if robot_enabled:
+    ROBOT_HOST = "192.168.1.100"  # Replace with your robot's IP
+    rtde_c = rtde_control.RTDEControlInterface(ROBOT_HOST)
+    rtde_r = rtde_receive.RTDEReceiveInterface(ROBOT_HOST)
+    rtde_io = r_io.RTDEIOInterface(ROBOT_HOST)
+    rtde_io.setSpeedSlider(ROBOT_SPEED)
+
 
 @sio.event
 def connect(sid, environ):
     print(f"Client {sid} connected.")
-    sio.emit(Events.SERVER_MESSAGE.value, {'status': 'Connected to Robot Arm Server'}, to=sid)
+    sio.emit(
+        Events.SERVER_MESSAGE.value, {"status": "Connected to Robot Arm Server"}, to=sid
+    )
+
 
 @sio.event
 def disconnect(sid):
     print(f"Client {sid} disconnected.")
 
 
+@sio.on(Events.IS_RUNNING.value)
+def is_running(sid):
+    return {"running": True}
 
-@sio.on(Events.MOVE_TO_POSITION.value)
-def move_to_position(sid, data):
-    try:
-        position = data.get("position", [])
-        speed = data.get("speed", 0.5)
-        acceleration = data.get("acceleration", 0.3)
 
-        if len(position) != 6:
-            raise ValueError("Invalid position format. Must be a list of 6 values.")
+@sio.on(Events.GET_TCP_VALUE.value)
+def get_tcp_pose(sid, data) -> List[float]:
+    positions = rtde_receive.getActualTCPPose()
+    return {"poistions": positions}
 
-        print(f"Moving to position: {position}")
-        rtde_control.moveJ(position, speed, acceleration, asynchronous=True)
-        sio.emit(Events.SERVER_MESSAGE.value, {'status': 'Movement complete'}, to=sid)
-    except Exception as e:
-        sio.emit(Events.SERVER_MESSAGE.value, {'status': f"Error: {str(e)}"}, to=sid)
 
-#@sio.on(Events.MOVE_TO_POSITION.value)
-def move_to_position_blend(sid, data):
-    try:
-        position = data.get("position", [])
-        speed = data.get("speed", 0.5)
-        acceleration = data.get("acceleration", 0.3)
-        blend = data.get("blend", 0.1)
+@sio.on(Events.SET_GRIPPER.value)
+def open_gripper(sid, data):
+    if robot_enabled:
+        rtde_io.setStandardDigitalOut(0, data["open"])
+    return {"open": data["open"]}
 
-        if len(position) != 6:
-            raise ValueError("Invalid position format. Must be a list of 6 values.")
 
-        print(f"Moving to position: {position}")
-        blended_position = []
-        blended_position.extend(position)
-        blended_position.extend([speed, acceleration, blend])
-        print(blended_position)
-        rtde_control.moveJ(blended_position, asynchronous=True)
-        print("Done moving")
-        sio.emit(Events.SERVER_MESSAGE.value, {'status': 'Movement complete'}, to=sid)
-        print("Done with emitting")
-    except Exception as e:
-        sio.emit(Events.SERVER_MESSAGE.value, {'status': f"Error: {str(e)}"}, to=sid)
+@sio.on(Events.GET_SPEED_VALUE.value)
+def get_tcp_velocity(sid):
+    return {"speed": ROBOT_SPEED}
 
-@sio.on(Events.GET_JOINT_POSITIONS.value)
-def get_joint_positions(sid):
-    try:
-        joint_positions = rtde_receive.getActualQ()
-        sio.emit(Events.JOINT_POSITIONS.value, {'positions': joint_positions}, to=sid)
-    except Exception as e:
-        sio.emit(Events.SERVER_MESSAGE.value, {'status': f"Error: {str(e)}"}, to=sid)
+
+@sio.on(Events.SET_SPEED_VALUE.value)
+def set_robot_velocity(self, data):
+    speed = data.get("speed", 0.5)
+    ROBOT_SPEED = speed
+    if robot_enabled:
+        rtde_io.setSpeedSlider(ROBOT_SPEED)
+    return {"speed": ROBOT_SPEED}
+
+
+@sio.on(Events.JOINT_POSITIONS.value)
+def get_actual_joint_positions(sid):
+    if robot_enabled:
+        return {"positions": np.degrees(rtde_r.getActualQ())}
+    return [1.0, 2.0, 3.0]
+
+
+@sio.on(Events.MOVE_TO_POSITION_J.value)
+def moveJ_path(sid, data):
+    path = data["positions"]
+    if robot_enabled:
+        rtde_c.moveJ(path, asynchronous=True)
+    return {"positions": path}
+
+
+@sio.on(Events.MOVE_TO_POSITION_L.value)
+def moveL_path(sid, data):
+    path = data["positions"]
+    if robot_enabled:
+        rtde_c.moveL(path, asynchronous=True)
+    return {"positions": path}
+
+
+@sio.on(Events.MOVE_L_SPEED_ACCEL.value)
+def moveL(sid, data):
+    target_pose = data["position"]
+    velocity = data["speed"]
+    acceleration = data["acceleration"]
+    if robot_enabled:
+        rtde_c.moveL(target_pose, speed=velocity, acceleration=acceleration)
+    return {"positions": target_pose}
 
 
 @sio.on(Events.STOP_ROBOT.value)
 def stop_robot(sid):
-    try:
-        rtde_control.stopL()
-        sio.emit(Events.SERVER_MESSAGE.value, {'status': 'Robot stopped'}, to=sid)
-    except Exception as e:
-        sio.emit(Events.SERVER_MESSAGE.value, {'status': f"Error: {str(e)}"}, to=sid)
+    if robot_enabled:
+        rtde_c.stopL()
 
 
-@sio.on(Events.SPEED_VALUE.value)
-def set_speed(sid, data):
-    try:
-        speed = data.get("speed", 0.5)
-        print(f"Speed being set is {speed}")
-        ROBOT_SPEED = speed
-        i_rtde_io.setSpeedSlider(ROBOT_SPEED)
-    except Exception as e:
-        sio.emit(Events.SERVER_MESSAGE.value, {'status': f"Error: {str(e)}"}, to=sid)
-
-@sio.on(Events.IS_RUNNING.value)
-def get_status(sid):
-    operation = rtde_control.getAsyncOperationProgress()
-    running = True 
-    if operation < 0:
-        running = False
-
-    sio.emit(Events.IS_RUNNING.value, {"running": running, "value": operation}, to=sid)
-
-@sio.on(Events.GET_TCP_VALUE.value)
-def get_tcp_value(sid):
-    sio.emit(Events.SERVER_MESSAGE.value, {"tcp_pose": rtde_receive.getActualTCPPose()})
-
-
-@sio.on(Events.SPEED_VALUE.value)
-def get_speed_value(sid):
-    sio.emit(Events.SPEED_VALUE.value, {"speed": ROBOT_SPEED}, to=sid)
-
-@sio.on(Events.SET_GRIPPER.value)
-def set_gripper_value(sid, data):
-    return i_rtde_io.setStandardDigitalOut(0, data["open"])
-
-def stop_server():
-    global server_thread
-    if server_thread is not None:
-        print("Stopping server...")
-        server_thread.kill()  # Stop the server greenthread
-        server_thread = None
-        print("Server stopped.")
+def stopServer():
+    os.kill(os.getpid(), signal.SIGINT)
 
 
 if __name__ == "__main__":
-    import eventlet
-    import eventlet.wsgi
+    # import eventlet
+    # import eventlet.wsgi
 
     try:
         print("Starting Socket.IO server...")
-        eventlet.wsgi.server(eventlet.listen(("0.0.0.0", 5000)), app)
+        # eventlet.wsgi.server(eventlet.listen(("0.0.0.0", 5000)), app)
+        app.run(port=5000)
     except KeyboardInterrupt:
-        stop_server()
-        
+        app.shutdown()
